be428792cce7373c52b6278ab41b92ff17881851
Malik <m.bagwala@gmail.com>
2019-09-21 10:06:46 +0530

    removed s3 plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-s3`,
       options: {
         bucketName: "creative-malik-portfolio",
       },
