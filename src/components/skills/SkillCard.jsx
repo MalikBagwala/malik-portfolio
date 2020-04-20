5b05d37277d935899838d045b2a52578aafb3ff8
MalikBagwala <m.bagwala@gmail.com>
2020-04-20 20:14:21 +0530

    added ratings component

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar } from "@fortawesome/free-solid-svg-icons"
 const Div = styled.div`
   height: 200px;
   width: 100%;
@@ -35,11 +36,30 @@ const IMG = styled.img`
   }
 `
 
+const Star = styled(FontAwesomeIcon)`
+  margin-top: 1rem;
+  margin-left: 3px;
+  margin-right: 3px;
+`
+
+const Rating = styled.div`
+  display: flex;
+`
+
 const SkillCard = ({ skill }) => {
+  function getRatings(rating) {
+    let ratings = []
+    for (let i = 0; i < rating; i++) {
+      ratings.push(<Star icon={faStar} />)
+    }
+    return ratings
+  }
   return (
     <Div color={skill.color}>
       <p>{skill.name}</p>
       <IMG color={skill.color} src={skill.logo.file.url} />
+
+      <Rating>{getRatings(skill.rating)}</Rating>
     </Div>
   )
 }
