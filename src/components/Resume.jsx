b27e02e837cc6678232e53f69fb76d7ec94ba3cf
MalikBagwala <malikeditz@gmail.com>
2020-05-04 14:57:38 +0530

    changed resume location

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,21 +4,13 @@ import ButtonLink from "./ButtonLink"
 import { faLink } from "@fortawesome/free-solid-svg-icons"
 import { StyledIcon } from "./WorkCard"
 const Resume = () => {
-  const res = useStaticQuery(graphql`
-    {
-      contentfulFiles(file: { description: { eq: "maliks-resume" } }) {
-        contentful_id
-        file {
-          file {
-            url
-          }
-        }
-      }
-    }
-  `)
-  const link = res && res.contentfulFiles.file.file.url
   return (
-    <ButtonLink href={link} card={false} purple={true}>
+    <ButtonLink
+      target="__blank"
+      href="https://github.com/MalikBagwala/creative-malik-portfolio/blob/master/assets/resume.pdf"
+      card={false}
+      purple={true}
+    >
       <StyledIcon icon={faLink} /> Resume
     </ButtonLink>
   )
