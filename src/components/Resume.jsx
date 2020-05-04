d87a2fd6b686cb583d03ee5f21f070ada0456f8c
MalikBagwala <malikeditz@gmail.com>
2020-05-04 15:01:11 +0530

    updated resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,12 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import ButtonLink from "./ButtonLink"
 import { faLink } from "@fortawesome/free-solid-svg-icons"
+import React from "react"
+import ButtonLink from "./ButtonLink"
 import { StyledIcon } from "./WorkCard"
 const Resume = () => {
   return (
     <ButtonLink
       target="__blank"
-      href="https://github.com/MalikBagwala/creative-malik-portfolio/blob/master/assets/resume.pdf"
+      href={`${window.location.href}resume.pdf`}
       card={false}
       purple={true}
     >
