221f6b832063a68650025f14b2797266983fc3c9
MalikBagwala <malikeditz@gmail.com>
2020-10-02 10:31:12 +0530

    Updated gatsby config js

--- src/components/templates/sidebar/sidebar.component.jsx
+++ src/components/templates/sidebar/sidebar.component.jsx
@@ -1,15 +1,43 @@
 import React from "react"
 import classNames from "../../../utils/classNames"
-import { sidebar } from "./sidebar.module.css"
+import { copyright, links, sidebar, title } from "./sidebar.module.css"
+import Icon from "../../atoms/icon/icon.component"
 const Sidebar = () => {
   return (
     <header
       className={classNames(
-        "bg-white border-r-2 p-4 border-gray-200 h-full fixed",
+        "bg-white border-r-2 py-12 px-8 border-gray-200 h-full fixed flex flex-col justify-between top-0 left-0 overflow-y-hidden",
         sidebar
       )}
     >
-      <h1 className="text-6xl font-display text-gray-700"> Hey guys</h1>
+      <div
+        className={classNames(
+          "text-4xl font-display flex flex-col text-center",
+          title
+        )}
+      >
+        <span className="uppercase tracking-wider">Malik</span>
+        <span className="text-base text-gray-600 tracking-wide">
+          front-end developer
+        </span>
+      </div>
+      <div
+        className={classNames(
+          "flex flex-col justify-center align-baseline",
+          copyright
+        )}
+      >
+        <div className={classNames("flex justify-center mb-4", links)}>
+          <Icon name="gitlab" className="h-4" />
+          <Icon name="github" className="h-4 ml-3" />
+          <Icon name="linkedln" className="h-4 ml-3" />
+          <Icon name="globe" className="h-4 ml-3" />
+        </div>
+        <p className="text-sm text-gray-600 text-center">
+          &copy; {new Date().getFullYear()}{" "}
+          <span className="text-gray-900">Malik Bagwala</span>
+        </p>
+      </div>
     </header>
   )
 }
