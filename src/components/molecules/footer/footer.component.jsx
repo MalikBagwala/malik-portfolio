a380ab40cf5bcdb029ff22425e293a7b3af2a1b3
MalikBagwala <malikeditz@gmail.com>
2020-10-08 15:01:00 +0530

    updated footer component

--- src/components/molecules/footer/footer.component.jsx
+++ src/components/molecules/footer/footer.component.jsx
@@ -1,26 +1,18 @@
 import React from "react"
 import classNames from "../../../utils/classNames"
-import { base } from "./footer.module.css"
-import { links } from "../../templates/sidebar/sidebar.module.css"
-import Icon from "../../atoms/icon/icon.component"
+import Social from "../social/social.component"
 const Footer = () => {
   return (
     <footer
       className={classNames(
-        "bg-gray-900 text-white px-12 py-16 flex justify-between items-center",
-        base
+        "bg-gray-900 text-white px-12 py-16 flex justify-between items-center"
       )}
     >
       <div className="text-xs text-white">
         &copy; {new Date().getFullYear()} MALIK. all rights reserved
       </div>
       <h1 className="text-4xl uppercase">Malik</h1>
-      <div className={classNames("flex justify-center mb-4", links)}>
-        <Icon name="gitlab" className="fill-current text-white h-4" />
-        <Icon name="github" className="fill-current text-white h-4 ml-3" />
-        <Icon name="linkedln" className="fill-current text-white h-4 ml-3" />
-        <Icon name="globe" className="fill-current text-white h-4 ml-3" />
-      </div>
+      <Social />
     </footer>
   )
 }
