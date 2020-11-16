71977276cedf2313a73c11d8c5bddcd6e8829aab
Malik Bagwala <m.bagwala@gmail.com>
2020-11-16 17:43:24 +0530

    Updated footer style

--- src/components/molecules/footer/footer.component.jsx
+++ src/components/molecules/footer/footer.component.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <footer
       className={classNames(
-        "bg-gray-900 text-white px-12 pt-8 pb-20 lg:py-16 flex flex-col lg:flex-row justify-between items-center"
+        "bg-gray-100 dark:bg-gray-900 text-white px-12 pt-8 pb-20 lg:py-16 flex flex-col lg:flex-row justify-between items-center"
       )}
     >
-      <div className="text-xs text-white order-1 lg:order-none">
+      <div className="text-xs text-gray-900 dark:text-white order-1 lg:order-none">
         &copy; {new Date().getFullYear()} MALIK. all rights reserved
       </div>
       <h1 className="hidden text-4xl uppercase">Malik</h1>
